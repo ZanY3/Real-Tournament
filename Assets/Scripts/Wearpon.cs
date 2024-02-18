77bdb94ae139c4af8ff962c8a82529cb2a38d54e
zany <cebureksteam@gmail.com>
2024-02-18 17:37:23 +0200

    Settings 2

--- Assets/Scripts/Wearpon.cs
+++ Assets/Scripts/Wearpon.cs
@@ -5,48 +5,52 @@ using UnityEngine;
 public class Wearpon : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public GameObject shootParticle;
+
     public int ammo;
     public int maxAmmo = 10;
     public bool isReloading;
     public bool isAutomatic;
     public float fireInterval = 0.1f;
     public float fireCooldown;
-    public float reloadTime;
+    public float reloadTime = 2;
 
-    private void Update()
+    void Update()
     {
-        //manual shooting
-        if (!isAutomatic && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Shoot();
-        }
-        //automatic shooting
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            // manual shooting
+            if (!isAutomatic && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Shoot();
+            }
+        // automatic shooting
         if (isAutomatic && Input.GetKey(KeyCode.Mouse0))
         {
             Shoot();
         }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             Reload();
-
         }
         fireCooldown -= Time.deltaTime;
     }
+
     void Shoot()
     {
         if (isReloading) return;
         if (ammo <= 0)
         {
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
             Reload();
             return;
         }
         if (fireCooldown > 0) return;
+
         ammo--;
         fireCooldown = fireInterval;
         Instantiate(bulletPrefab, transform.position, transform.rotation);
-        Instantiate(shootParticle, transform.position, transform.rotation);
     }
+
     async void Reload()
     {
         if (ammo == maxAmmo) return;
@@ -54,7 +58,11 @@ public class Wearpon : MonoBehaviour
 
         isReloading = true;
 
-        print("reload");
-        await new WaitForSeconds();
+        print("Reloading...");
+        await new WaitForSeconds(reloadTime);
+        print("Reloaded!");
+
+        isReloading = false;
+        ammo = maxAmmo;
     }
 }
