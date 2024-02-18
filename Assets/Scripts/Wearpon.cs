f087fd168acec6000205a149b159fdd706c6e8a5
zany <cebureksteam@gmail.com>
2024-02-18 17:33:35 +0200

    Reload, wearpon settings

--- Assets/Scripts/Wearpon.cs
+++ Assets/Scripts/Wearpon.cs
@@ -6,12 +6,55 @@ public class Wearpon : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject shootParticle;
+    public int ammo;
+    public int maxAmmo = 10;
+    public bool isReloading;
+    public bool isAutomatic;
+    public float fireInterval = 0.1f;
+    public float fireCooldown;
+    public float reloadTime;
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        //manual shooting
+        if (!isAutomatic && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(bulletPrefab, transform.position, transform.rotation);
-            Instantiate(shootParticle, transform.position, transform.rotation);
+            Shoot();
         }
+        //automatic shooting
+        if (isAutomatic && Input.GetKey(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+
+        }
+        fireCooldown -= Time.deltaTime;
+    }
+    void Shoot()
+    {
+        if (isReloading) return;
+        if (ammo <= 0)
+        {
+            Reload();
+            return;
+        }
+        if (fireCooldown > 0) return;
+        ammo--;
+        fireCooldown = fireInterval;
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Instantiate(shootParticle, transform.position, transform.rotation);
+    }
+    async void Reload()
+    {
+        if (ammo == maxAmmo) return;
+        if (isReloading) return;
+
+        isReloading = true;
+
+        print("reload");
+        await new WaitForSeconds();
     }
 }
