39a65998ef5caa233c1717d4b7d50681ff209069
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-25 15:09:49 +0200

    Upgraded Health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,40 +3,38 @@ using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    public int hp;
-    public int maxHp = 100;
-    public bool shouldDestroy;
-
-    public UnityEvent onDie;
-    public UnityEvent onDamage;
+    public int health;
+    public int maxHealth = 100;
+    public bool shouldDestroy = true;
 
     public GameObject damageEffect;
     public GameObject deathEffect;
 
+    public UnityEvent onDamage;
+    public UnityEvent onDie;
+
     void Start()
     {
-        if (hp == 0) hp = maxHp;
+        if(health == 0) health = maxHealth;
     }
+
 
     public void Damage(int damage)
     {
-        
-        hp -= damage;
+        health -= damage;
         onDamage.Invoke();
-        if (hp <= 0)
+        if(damageEffect != null) Instantiate(damageEffect, transform.position, Quaternion.identity);
+        if(health <= 0)
         {
             Die();
         }
-        if (hp < 0) hp = 0;
-
-        if (damageEffect != null) Instantiate(damageEffect, transform.position, Quaternion.identity);
-        onDamage.Invoke();
+        if( health < 0) health = 0;
     }
 
     public void Die()
     {
-        if (deathEffect != null) Instantiate(deathEffect, transform.position, Quaternion.identity);
         onDie.Invoke();
-        if (shouldDestroy) Destroy(gameObject);
+        if(shouldDestroy)Destroy(gameObject);
+        if(deathEffect != null) Instantiate(deathEffect, transform.position, Quaternion.identity);
     }
 }
